edc4f9582dcdb4175d6bc391b7c7c84e24451e62
JennieJT <jingtianwangwork@gmail.com>
2024-04-02 17:16:33 -0400

    initialization

--- src/indexAfter.js
+++ src/indexAfter.js
@@ -40,10 +40,10 @@ function handleAMouseLeave(e) {
     const mouseParameterAll = document.getElementById("mouseParameterAll");
     mouseParameter.style.display = "block";
     mouseParameterAll.style.display = "block";
- 
+
     mouseParameter.innerHTML = text;
-    let prev = mouseParameterAll.innerHTML; 
-    mouseParameterAll.innerHTML =  prev + `${whitespace} ` + text;
+    let prev = mouseParameterAll.innerHTML;
+    mouseParameterAll.innerHTML = prev + `${whitespace} ` + text;
 }
 
 /**
@@ -73,8 +73,8 @@ function handleAMouseMove(e) {
     mouseParameter.style.display = "block";
     mouseParameterAll.style.display = "block";
     mouseParameter.innerHTML = text;
-    let prev = mouseParameterAll.innerHTML; 
-    mouseParameterAll.innerHTML =  prev + `${whitespace} ` + text;
+    let prev = mouseParameterAll.innerHTML;
+    mouseParameterAll.innerHTML = prev + `${whitespace} ` + text;
 }
 
 /**
@@ -105,35 +105,95 @@ function handleAMouseEnter(e) {
     mouseParameterAll.style.display = "block";
     mouseParameter.innerHTML = text;
     mouseParameterAll.textContent = "";
-    mouseParameterAll.innerHTML =  "<br>" + text;
+    mouseParameterAll.innerHTML = "<br>" + text;
 }
 
-(function doNotJump(){
+(function doNotJump() {
     let allInputs = document.querySelectorAll("#form input");
+
     allInputs.forEach((input) => {
         input.addEventListener("keydown", (event) => {
             event.key === "Enter" && event.preventDefault();
-        } )
+        })
     });
-})
+}())
 
-function handleFormSubmit(){
+
+
+function handleFormSubmit(e) {
+    e.preventDefault()
     const formValues = {};
     let allInputs = document.querySelectorAll("#form input")
 
     allInputs.forEach((input) => {
-    /**
-     * @type {HTMLInputElement}
-     */
-    let tmp = input;
-    if(formValues[tmp.name]){
-        const valueArr = [...formValues[tmp.name]];
-        valueArr.push(tmp.value);
-    }
-    formValues[tmp.name] = tmp.value;
+        /**
+         * @type {HTMLInputElement}
+         */
+        let tmp = input;
+
+        // check checkbox
+        if (tmp.type === "checkbox") {
+            if (tmp.checked) {
+                if (!formValues[tmp.name]) {
+                    formValues[tmp.name] = [];
+                }
+                formValues[tmp.name].push(tmp.value);
+            }
+        } else if (tmp.type === "radio") {
+            tmp.checked && (formValues[tmp.name] = tmp.value);
+        } else if (tmp.type === "submit" || tmp.type === "reset" || tmp.type === "button") {
+        } else {
+            formValues[tmp.name] = tmp.value;
+        }
+
     });
+
+    //textareas
+    const allTextAreas = document.querySelectorAll("#form textarea");
+    allTextAreas.forEach((it) => {
+        formValues[it.name] = it.value;
+    });
+
+    //options
+    const allOptions = document.querySelectorAll("#form select");
+    allOptions.forEach((it) => {
+        formValues[it.name] = it.value;
+    });
+
+
     console.log("values:" + formValues);
-    for(let v in formValues){
+    for (let v in formValues) {
         console.log(v + " " + formValues[v]);
     }
+
+    localforage.setItem('jtFormValues', formValues);
+}
+
+function handleFormOnLoad(e) {
+    /**
+     * @type {object}
+     */
+    let initData;
+    localforage.getItem('jtFormValues').then((it) => {
+        initData = it;
+        const allInputs = document.querySelectorAll("#form input");
+        for (let key in initData) {
+            console.log(key + " " + initData[key])
+            allInputs.forEach((tmp) => {
+                if (tmp.type === "checkbox" && key.toLowerCase().includes("checkbox")) {
+                    if(initData[key].includes(tmp.value))
+                        tmp.checked = true;
+                } else if (tmp.type === "radio" && key.toLowerCase().includes("radio")) {
+                    if(tmp.value === initData[key]){
+                        tmp.checked = true;
+                    }
+                } else if (tmp.name === key) {
+                    tmp.value = initData[key];
+                } else {
+                    tmp.value === "";
+                   // tmp.checked && (tmp.checked = false);
+                }
+            });
+        }
+    });
 }
